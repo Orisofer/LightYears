d8c4bf220b442bd07461fa5b5bbc3461a8de54ba
Ori Sofer <orisofercetem@gmail.com>
2025-02-16 00:42:42 +0200

    good luck message

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,7 +1,9 @@
-
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello Vorleeed" << std::endl;
+    std::cout << "Hello cool" << std::endl;
+    std::cout << "Hello good luck" << std::endl;
+    std::cout << "Hello with project" << std::endl;
 }
