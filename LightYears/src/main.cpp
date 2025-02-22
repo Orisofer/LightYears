817707ae8e8f990d01f9199fac75ae21579775de
Ori Sofer <orisofercetem@gmail.com>
2025-02-22 12:58:21 +0200

    testing with rendering a window

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -3,7 +3,23 @@
 
 int main()
 {
-    sf::RenderWindow renderWindow{sf::VideoMode(1000,1000), "My Window"};
+    sf::RenderWindow renderWindow{sf::VideoMode(200,500), "My Window"};
+    
+    while (renderWindow.isOpen())
+    {
+        sf::Event event;
+        
+        while (renderWindow.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                renderWindow.close();
+            }
+            
+            renderWindow.clear(sf::Color::Blue);
+            renderWindow.display();
+        }
+    }
     
     std::cout << "Hello" << std::endl;
 }
