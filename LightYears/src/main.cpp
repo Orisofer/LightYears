6d67176ea1240bfbf532fdc6f9a5242516ed8ffe
Ori Sofer <orisofercetem@gmail.com>
2025-02-22 14:32:56 +0200

    changed application run to made with smart ptr

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <memory>
 #include <SFML/Graphics.hpp>
+
 #include "framework/Application.h"
+
 
 int main()
 {
-    ly::Application* app = new ly::Application();
-    
+    std::unique_ptr<ly::Application> app = std::make_unique<ly::Application>();
     app->Run();
     
     std::cout << "Hello" << std::endl;
