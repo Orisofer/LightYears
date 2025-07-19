ad3f9b0b9d5e0404bde8675af56d11123b6340ca
Ori Sofer <orisofercetem@gmail.com>
2025-07-19 11:45:54 +0300

    added the player class to spawn spaceships from there

---
+++ LightYearsGame/include/player/Player.h
@@ -0,0 +1,40 @@
+//
+// Created by Ori Sofer on 19/07/2025.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "PlayerSpaceship.h"
+#include "framework/Object.h"
+#include "framework/Delegate.h"
+
+namespace ly
+{
+    class PlayerSpaceship;
+
+    class Player : public Object
+    {
+    public:
+        Player();
+        weak<PlayerSpaceship> SpawnSpaceship(World* world);
+        const weak<PlayerSpaceship> GetSpaceship() const;
+
+        void AddLifeCount(unsigned int count);
+        unsigned int GetLifeCount() const;
+
+        void AddScore(unsigned int score);
+        unsigned int GetScore() const;
+
+        Delegate<int> m_OnLifeChanged;
+        Delegate<int> m_OnScoreChanged;
+        Delegate<> m_OnLifeExhausted;
+
+    private:
+        weak<PlayerSpaceship> m_CurrentSpaceship;
+        unsigned int m_LifeCount;
+        unsigned int m_Score;
+    };
+}
+
+#endif //PLAYER_H
---
+++ LightYearsGame/src/player/Player.cpp
@@ -0,0 +1,68 @@
+//
+// Created by Ori Sofer on 19/07/2025.
+//
+
+#include "player/Player.h"
+#include "framework/World.h"
+#include "player/PlayerSpaceship.h"
+
+namespace ly
+{
+    Player::Player() :
+    m_LifeCount(3),
+    m_Score(0)
+    {
+    }
+
+    weak<PlayerSpaceship> Player::SpawnSpaceship(World *world)
+    {
+        if (m_LifeCount > 0)
+        {
+            m_LifeCount--;
+
+            auto windowSize = world->GetWindowSize();
+
+            m_CurrentSpaceship = world->SpawnActor<PlayerSpaceship>();
+
+            m_CurrentSpaceship.lock()->SetLocation(sf::Vector2f(windowSize.x / 2.f,
+                windowSize.y - 100.f));
+
+            m_OnLifeChanged.Broadcast(GetLifeCount());
+        }
+        else
+        {
+            m_OnLifeExhausted.Broadcast();
+        }
+
+        return m_CurrentSpaceship;
+    }
+
+    const weak<PlayerSpaceship> Player::GetSpaceship() const
+    {
+        return m_CurrentSpaceship;
+    }
+
+    void Player::AddLifeCount(unsigned int count)
+    {
+        m_LifeCount += count;
+
+        m_OnLifeChanged.Broadcast(GetLifeCount());
+    }
+
+    unsigned int Player::GetLifeCount() const
+    {
+        return m_LifeCount;
+    }
+
+    void Player::AddScore(unsigned int score)
+    {
+        m_Score += score;
+
+        m_OnScoreChanged.Broadcast(GetScore());
+    }
+
+    unsigned int Player::GetScore() const
+    {
+        return m_Score;
+    }
+}
